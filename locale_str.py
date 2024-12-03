f016ba747b3d4bf1031455d1e2ae66cfa7ce2e25
Michal <mprilepok@gmail.com>
2024-12-03 22:12:29 +0100

    First version

---
+++ locale_str.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2021 SQ9MDD Rysiek Labus
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+en_awt = ['',
+        'strong wind',
+        'snow warning',
+        'thunderstorm warning',
+        'fog warning',
+        'extreme high temperature',
+        'extreme low temperature',
+        'coastal event warning',
+        'forest fire warning',
+        'Avalanches warning',
+        'Strong rain warning',
+        'Flood warning',
+        'Rain-Flood warning']
+
+en_lvl = ['No valid alerts',
+        'NO WEATHER ALERTS',
+        'YELLOW ALERT',
+        'ORANGE ALERT',
+        'RED ALERT']
+
+# es written by EA8CXN Cesar
+es_awt = ['',
+        'Vientos',
+        'Nevadas',
+        'Tormentas',
+        'Nieblas',
+        'Temperaturas maximas',
+        'Temperaturas minimas',
+        'Fenomenos costeros',
+        'Riesgo de incendio',
+        'Avalanches warning',
+        'Lluvias',
+        'Riesgo de inundacion',
+        'Riesgo desborde']
+
+es_lvl = ['No valid alerts',
+        'Nivel Verde',
+        'Nivel Amarillo',
+        'Nivel Naranja',
+        'Nivel Rojo']
+
+# pl written by SQ9MDD Rysiek
+pl_awt = ['',
+        'silny wiatr',
+        'silne opady sniegu',
+        'zagrozenie burzowe',
+        'silna mgla',
+        'upal',
+        'silny mroz',
+        'wysokie fale',
+        'zagrozenie pozarami lasow',
+        'zagrozenie lawinowe',
+        'silny deszcz',
+        'zagrozenie powodziowe',
+        'silne deszcze i podtopienia']
+
+pl_lvl = ['brak poprawnych danych ',
+        'BRAK ALARMOW POGODOWYCH',
+        'ZOLTY ALARM',
+        'POMARANCZOWY ALARM',
+        'CZERWONY ALARM']
+
+# sk written by OM1PU Michal
+sk_awt = ['',
+        'silný vietor',
+        'výstraha pred snežením',
+        'varovanie pred búrkou',
+        'varovanie pred hmlou',
+        'extrémne vysoké teploty',
+        'extrémne nízka teplota',
+        'výstraha pred pobrežnými udalosťami',
+        'výstraha pred lesným požiarom',
+        'varovanie pred lavínami',
+        'varovanie pred silným dažďom',
+        'varovanie pred povodňami',
+        'Výstraha pred dažďom a povodňami']
+
+sk_lvl = ['ŽIADNE UPOZORNENIA NA POČASIE',
+        'ŽLTÁ VÝSTRAHA',
+        'ORANŽOVÁ VÝSTRAHA',
+        'ČERVENÁ VÝSTRAHA']
--- meteoalarm.py
+++ meteoalarm.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-# Copyright (c) 2021 SQ9MDD Rysiek Labus
+# Copyright (c) 2024 OM1PU Michal Prilepok
+# Based on work SQ9MDD Rysiek Labus
 #
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
 # DATA SOURCE meteoalarm.eu
-# look for metegram and find" img with alt="awt:6 level:1"
+# look for metegram and find' img with alt='awt:6 level:1'
 #   Awarness Type:
 #   1       wind
 #   2       snow / ice
@@ -29,53 +30,125 @@
 #   4       RED The weather is very dangerous. Exceptionally intense meteorological phenomena have been forecast. Major damage and accidents are likely, in many cases with threat to life and limb, over a wide area.
 
 # CHANGELOG
-# 20210707 - changing feed source according to new meteoalarm interface (https://feeds.meteoalarm.org/) tnx for info Cesar ;-)
-# 20220118 - deploy fixes from SQ7LQU
-# 20230807 - migrating to python3
-# ----------------------configuration----------------------------- #
-rss_url = 'https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-rss-poland'         # put here valid RSS url for your country from meteoalarm.eu
-#rss_county = 'Mazowieckie Province Warszawa County'                                 # region or country name exactly as it is in RSS
-rss_county = 'Zachodniopomorskie Province Świnoujście County'
+# 20241202 - fork from SQ9MDD Rysiek Labus repo
+# 20241203 - Added aprs, extende configuraion, sk translation
 
-from locale import pl_awt as def_awt
-from locale import pl_lvl as def_lvl
-# ----------------------configuration----------------------------- #
-
+from locale_str import sk_lvl as def_lvl
+from locale_str import sk_awt as def_awt
 from urllib.request import urlopen
+import aprslib
+import logging
+from datetime import datetime
+# ----------------------configuration----------------------------- #
+# put here valid RSS url for your country from meteoalarm.eu
+rss_url = 'https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-rss-slovakia'
+# region or country name exactly as it is in RSS
+rss_county = 'Senec'
+# aprs
+callsign = 'NOCALL-6'
+passwd = '12345'
+host = 'euro.aprs2.net'
+bulletin_name = 'BLN1WXSC' # bulletin name for Senec
+port = 14580
+timestamp_format = '%d.%m. %H:%M'
 
-def set_label_for_alert(alert = 0):
-    if(alert == 0):
-        return()
+debug_level = logging.ERROR
+# ----------------------configuration----------------------------- #
+
+
+def set_label_for_alert(alert=0):
+    if alert < 0 and alert < 12:
+        return ()
     else:
-        return(def_lvl[alert])
+        return def_lvl[alert]
 
-def set_label_for_awerness(awt = 0):
-    if(awt == 0):
-        return()
+
+def set_label_for_awerness(awt=0):
+    if awt < 0 and awt > 4:
+        return ()
     else:
-        return(def_awt[awt])
+        return def_awt[awt]
 
-def get_data_and_extract_alerts(rss_url,rss_county):
+
+def get_data_and_extract_alerts(rss_url, rss_county):
     try:
         response = urlopen(rss_url)
         text = response.read().decode('utf-8')
         county_code_pos = text.find(rss_county)
-        awt_pos = text.find("awt:",county_code_pos) + 4
-        lvl_pos = text.find("level:",county_code_pos) + 6
-        awt = text[awt_pos:(awt_pos + 2)]        # AWT
-        lvl = text[lvl_pos:(lvl_pos + 1)]        # LVL
-        return_data = [int(awt),int(lvl)]        # awt,lvl
-        return(return_data)
-    except:
-        return_data = [0,0]
-        return()
+        awt_pos = text.find('awt:', county_code_pos) + 4
+        lvl_pos = text.find('level:', county_code_pos) + 6
+        from_pos = text.find('From:', county_code_pos) + 13
+        until_pos = text.find('Until:', county_code_pos) + 14
+        awt = text[awt_pos: (awt_pos + 2)]  # AWT
+        lvl = text[lvl_pos: (lvl_pos + 1)]  # LVL
+        from_time = text[from_pos: (from_pos + 25)]  # from_time
+        until_time = text[until_pos: (until_pos + 25)]  # until_time
 
+        # awt,lvl,from_time,until_time
+        return_data = [int(awt), int(lvl), from_time, until_time]
+        return return_data
+    except:
+        # awt,lvl,from_time,until_time
+        return_data = [0, 0, '', '']
+        return return_data
+
+
+def create_status_frame(awt, lvl, from_time, until_time):
+    status_frame = (
+        f'{callsign}>APRS,TCPIP*:>'
+        + set_label_for_alert(lvl)
+        + ' '
+        + set_label_for_awerness(awt)
+        + ' ('
+        + datetime.fromisoformat(from_time).strftime(timestamp_format)
+        + ' - '
+        + datetime.fromisoformat(until_time).strftime(timestamp_format)
+        + ' UTC) (https://meteoalarm.org)'
+    )
+
+    return status_frame
+
+
+def create_bulletin_frame(awt, lvl, from_time, until_time):
+    status_frame = (
+        f'{callsign}>APRS,TCPIP*::{bulletin_name.ljust(5)}:Okres {rss_county}'
+        + set_label_for_alert(lvl)
+        + ' '
+        + set_label_for_awerness(awt)
+        + ' ('
+        + datetime.fromisoformat(from_time).strftime(timestamp_format)
+        + ' - '
+        + datetime.fromisoformat(until_time, ).strftime(timestamp_format)
+        + ' UTC) (https://meteoalarm.org)'
+    )
+
+    return status_frame
+
+
+logging.basicConfig(level=debug_level)
 try:
-    curr_alert_data = get_data_and_extract_alerts(rss_url,rss_county)
-    if(curr_alert_data[1] > 1):
-        status_frame = ">" + set_label_for_alert(curr_alert_data[1]) + ' ' + set_label_for_awerness(curr_alert_data[0])
+    aprs_client = aprslib.IS(
+        callsign=callsign, passwd=passwd, host=host, port=port)
+    aprs_client .connect()
+
+    curr_alert_data = get_data_and_extract_alerts(rss_url, rss_county)
+
+    if curr_alert_data[1] > 1:
+        status_frame = create_status_frame(
+            curr_alert_data[0], curr_alert_data[1], curr_alert_data[2], curr_alert_data[3])
     else:
-        status_frame = ">" + set_label_for_alert(curr_alert_data[1])
-    print(status_frame)
-except:
+        status_frame = f'{callsign}>APRS,TCPIP*:' + \
+            '>' + set_label_for_alert(curr_alert_data[1])
+
+    aprs_client .sendall(status_frame)
+
+    if curr_alert_data[1] > 1:
+        status_frame = create_bulletin_frame(
+            curr_alert_data[0], curr_alert_data[1], curr_alert_data[2], curr_alert_data[3])
+
+        aprs_client .sendall(status_frame)
+
+    aprs_client .close()
+except Exception as e:
+    logging.error(e)
     pass
